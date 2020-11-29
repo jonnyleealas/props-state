70026e85d62519efd4c1ed9dd75a48092ef7d361
jonathon lee <jonnyleealas@gmail.com>
2020-11-29 16:22:57 -0600

    add functions

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component{
+  constructor(props){
+    super(props)
+    this.state ={
+      balance: 100
+    }
+  }
+  withdraw = (amount)=>{
+    this.setState( { balance: this.state.balance - amount})
+  }
+  addMoney = (amount)=>{
+    this.setState( { balance: this.state.balance + amount})
+  }
+  add20 = (amount)=>{
+    this.setState( {balance: this.state.balance + amount})
+  }
+  take20 = (amount)=>{
+    this.setState( {balance: this.state.balance - amount})
+  }
+  render(){
+    return(
+      <>
+      <h1> Bank Balance Is:${this.state.balance}</h1>
+      <button onClick={()=>this.withdraw(1)}>Withdraw Money</button>
+      <button onClick={ ()=>this.addMoney(1)}>Add Money</button>
+      <button onClick={ ()=>this.add20(20)}>Deposite 20</button>
+      <button onClick={ ()=>this.take20(20)}>Withdraw 20</button>
+      </>
+    ) 
+  }
 }
 
 export default App;
